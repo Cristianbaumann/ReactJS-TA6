86e0d0d1502b3ba260903d36f7c7153ac893df71
Cristianbaumann <cristian.baumann@correo.ucu.edu.uy>
2024-09-24 17:31:59 -0300

    cambios

---
+++ react-ta6/src/Componentes/Input.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+
+function TaskList() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task.trim()) {
+      setTasks([...tasks, task]);
+      setTask(''); // Limpiar input después de agregar la tarea
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Ingresa una tarea"
+      />
+      <button onClick={addTask}>Agregar Tarea</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TaskList;
